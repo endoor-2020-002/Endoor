c4f9af1cd2d40fac11b4e300d06228353e302faa
Govert <endoor-2020-002@outlook.com>
2020-03-29 19:41:05 +0200

    Input output opdracht

---
+++ InputOutput/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // BMI calculator
+            string gewichtInput;
+            string lengteInput;
+            double gewicht; // in kg
+            double lengteCM;
+            double lengteM;
+            double bmi;
+
+            Console.WriteLine("Hoeveel weeg je? (in kg) ");
+            gewichtInput = Console.ReadLine();
+            gewicht = Convert.ToDouble(gewichtInput);
+
+            Console.WriteLine("Hoe lang ben je? (in cm)");
+            lengteInput = Console.ReadLine();
+            lengteCM = Convert.ToDouble(lengteInput);
+            lengteM = lengteCM * 0.01;
+
+            bmi = gewicht / (lengteM * lengteM);
+
+            Console.WriteLine($"Je BMI is {bmi}");
+
+        }
+    }
+}
