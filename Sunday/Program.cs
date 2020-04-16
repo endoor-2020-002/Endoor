1fef8bb0f6edda44975429a1ce85b30a291d8b4c
Govert <endoor-2020-002@outlook.com>
2020-04-16 16:09:52 +0200

    Sunday!

---
+++ Sunday/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sunday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i <= 100; i++) 
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                    Console.WriteLine("Sunday!");
+                else if (i % 3 == 0)
+                    Console.WriteLine("Sun!");
+                else if (i % 5 == 0)
+                    Console.WriteLine("Day!");
+                else
+                    Console.WriteLine($"{i}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
