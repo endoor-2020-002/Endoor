1349e23ab1f9cc5987452aee0291f3317c1c6567
Govert <endoor-2020-002@outlook.com>
2020-06-04 14:08:37 +0200

    stringconverterstart

---
+++ StringConverter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class StringConverter
+    {
+        //fields
+        string inputString;
+
+        //constructors
+
+        //methods
+
+        public static string Reverse(string str)
+        { 
+        }
+
+        public static bool IsPalindrome(string str)
+        { 
+        }
+
+        public static string PigLatinate(string str)
+        { 
+        }
+
+        public static string SHortHand(string str)
+        { 
+        }
+
+
+    }
+}
