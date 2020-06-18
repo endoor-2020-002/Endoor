00b0fb6c23216210dfa2c1489380aa17c06525fc
Govert <endoor-2020-002@outlook.com>
2020-06-18 10:04:26 +0200

    TOp2000 Govert project

---
+++ TOP2000Govert/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TOP2000Govert.MvcApplication" Language="C#" %>
---
+++ TOP2000Govert/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TOP2000Govert.Startup))]
+namespace TOP2000Govert
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
